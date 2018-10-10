3ec3c63a571b351075e0cb3b1c5bf2f19c53723e
Miguel Muscat <miguelmuscat93@gmail.com>
2018-10-10 11:11:20 +0200

    Added offset param to `query()`

--- src/QueryCapableManagerInterface.php
+++ src/QueryCapableManagerInterface.php
@@ -24,11 +24,13 @@ interface QueryCapableManagerInterface
      *                                            additional keys may be accepted that are not entity data keys.
      * @param int|null                   $limit   Optional maximum number of entities to return, or null for no limit.
      *                                            Note that implementations may still impose a limit for large sets.
+     * @param int|null                   $offset  Optional number of entities to offset for the result, or null for no
+     *                                            offset. Offsets are applied after the $limit.
      * @param string|Stringable          $orderBy Optional name of the entity property by which to sort the returned
      *                                            entities, or null for no sorting.
      * @param bool                       $desc    Optional flag which if true will sort the entities in descending
      *                                            order. If false, which is default, returned entities are sorted in
      *                                            ascending order. Only applicable if the $orderBy param is not null.
      */
-    public function query($query = array(), $limit = null, $orderBy = null, $desc = false);
+    public function query($query = array(), $limit = null, $offset = null, $orderBy = null, $desc = false);
 }
